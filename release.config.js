daa695ccfdf754b35ae918eefd1f1a57fe492090
Y0ug0 <hugo.contreras.pro@gmail.com>
2021-01-05 20:02:21 +0100

    Change CI/CD config

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    branch: 'master',
+    repositoryUrl: 'git@github.com:hy0ug0/bic.git',
+    verifyConditions: ['@semantic-release/npm'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    { type: 'docs', scope: 'README', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'ci', release: 'patch' },
+                ],
+            },
+        ],
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: true,
+            },
+        ],
+    ],
+};
